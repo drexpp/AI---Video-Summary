e32f9199752aa533e05412894d3216b0c3da8c6e
Iván De los Santos García <Drexpp@MacBook-Pro-de-Ivan.local>
2018-09-05 19:09:38 +0200

    Media valores

--- prueba.py
+++ prueba.py
@@ -24,14 +24,34 @@ def recorre_imagenes():
             histr = cv2.calcHist([img],[i],None,[256],[0,256])
             plt.plot(histr, color = col)
             plt.xlim([0,256])
-            histrRecord.append(histr)
+            print('-----Color name --> '+ str(col))
+            print(histr)
+            SummaryOfColor = summary_color(histr)
+            histrRecord.append(SummaryOfColor)
+
+        
+        print('-------New image---------')
+       
         
         listaFrames.append([input_path,histrRecord])
-
+        print(listaFrames)
 
         plt.show()
-    print(listaFrames)
 
+
+def summary_color(histr):
+    result = 0
+    colorSum = 0
+    index = 0
+    for idx, value in enumerate(histr):
+        colorSum += value
+        index = idx
+
+    result = colorSum/(index+1)
+    print('Color sum: ' + str(colorSum))
+    print('index: '+ str(index+1))
+    print('result: '+ str(result))
+    return result
 
 if __name__ == '__main__':
     recorre_imagenes()
